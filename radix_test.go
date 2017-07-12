40fd3831763fe51de56b15fc080bb2773198438a
Eric Lagergren <ericscottlagergren@gmail.com>
2017-07-12 13:30:29 -0700

    move testing data to dedicated directory, add benchmark

--- radix_test.go
+++ radix_test.go
@@ -3,56 +3,83 @@ package drt
 import (
 	"bufio"
 	"compress/gzip"
-	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 	"testing"
+)
+
+const pwlist = "password_list.db"
+
+var (
+	wordList = makeWords()
+	gt       *Trie
+	honeynet = filepath.Join("_testdata", "honeynet.txt.gz")
 )
 
 func TestMain(m *testing.M) {
 	setup()
 	var err error
-	gt, err = Open("password_list.db")
+	gt, err = Open(pwlist)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 	status := m.Run()
 	err = gt.Close()
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
-	os.Remove("password_list.db")
+	os.Remove(pwlist)
 	os.Exit(status)
 }
 
 func setup() {
-	_, err := os.Stat("password_list.db")
-	if !os.IsNotExist(err) {
-		return
+	if _, err := os.Stat(pwlist); err != nil && !os.IsNotExist(err) {
+		panic(err)
 	}
-	t, err := Create("password_list.db")
+	t, err := Create(pwlist)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 	defer t.Close()
-	file, err := os.Open("honeynet.txt.gz")
+	file, err := os.Open(honeynet)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 	defer file.Close()
 	r, err := gzip.NewReader(file)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 	defer r.Close()
 	s := bufio.NewScanner(r)
 	for s.Scan() {
 		t.Insert(s.Text())
 	}
-	err = s.Err()
-	if err != nil {
-		log.Fatalln(err)
+	if err := s.Err(); err != nil {
+		panic(err)
 	}
+}
+
+func makeWords() [][]byte {
+	file, err := os.Open(honeynet)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	s := bufio.NewScanner(r)
+
+	var words [][]byte
+	for s.Scan() {
+		words = append(words, []byte(s.Text()))
+	}
+	return words
 }
 
 func randslice(t *testing.T) []byte {
@@ -60,37 +87,36 @@ func randslice(t *testing.T) []byte {
 	_, err := rand.Read(buf[:])
 	if err != nil {
 		if t == nil {
-			log.Fatalln(err)
+			panic(err)
 		}
 		t.Fatal(err)
 	}
 	return buf[:]
 }
 
-var gt *Trie
-
 func TestHas(t *testing.T) {
-	file, err := os.Open("honeynet.txt.gz")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-	r, err := gzip.NewReader(file)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer r.Close()
-	s := bufio.NewScanner(r)
-	for i := 1; s.Scan(); i++ {
-		if !gt.Has(s.Bytes()) {
-			t.Fatalf("#%d: (%q) was not found", i, s.Bytes())
+	const N = 50000
+	for i := 0; i < N; i++ {
+		word := wordList[i%len(wordList)]
+		if !gt.Has(word) {
+			t.Fatalf("#%d: (%q) was not found", i, word)
 		}
 	}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < N; i++ {
 		rs := randslice(t)
 		if gt.Has(rs) {
 			t.Fatalf("randslice (%#v) was found", rs)
 		}
 	}
+}
+
+var ghas bool
+
+func BenchmarkHas(b *testing.B) {
+	var lhas bool
+	for i := 0; i < b.N; i++ {
+		lhas = gt.Has(wordList[i%len(wordList)])
+	}
+	ghas = lhas
 }
